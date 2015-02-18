6b6e05a98805aabe111d6e84f31eab68afbea0ab
Zammel <medalizml@gmail.com>
2015-02-19 00:11:32 +0100

    add bank ,share and company(private,public)
    Signed-off-by: Zammel <medalizml@gmail.com>

--- BluesClient/src/main/java/tn/esprit/BluesClient/Screeners/companyCTRL.java
+++ BluesClient/src/main/java/tn/esprit/BluesClient/Screeners/companyCTRL.java
@@ -7,7 +7,20 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javax.imageio.ImageIO;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
+
+
+
+import tn.esprit.Blues.Services.BankShareServices;
+import tn.esprit.Blues.Services.CompanyShareService;
+import tn.esprit.Blues.entities.Bank;
+import tn.esprit.Blues.entities.Bond;
+import tn.esprit.Blues.entities.Company;
+import tn.esprit.Blues.entities.Sector;
+import tn.esprit.Blues.entities.Share;
 import tn.esprit.BluesClient.Main.ScreensFramework;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
@@ -16,7 +29,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -48,28 +64,146 @@ public class companyCTRL implements Initializable, ControlledScreen {
 	Tab BankList;
 	FileChooser fileChooser;
 	File file;
+	
+	
+
+	
+	
+	
 	@FXML
-	Button openButtonPublic;
+	Pane CompanyPane;
+	
 	@FXML
-	Button openButtonPrivate;
+	ImageView home;
+	//*****************start form add company  public *********************
 	@FXML
-	Button openButtonBank;
+	Button addCompanyPublicB;
 	@FXML
-	ImageView logoPrivate;
+	TextField namePublicCompany;
 	@FXML
-	ImageView logoBank;
+	DatePicker DatePublicCompany;
+	@FXML
+	TextField CodePublicCompany;
+	@FXML
+	TextField headPublicCompany;
+	@FXML
+	TextField PhonePublicCompany;
+	@FXML
+	TextField socialPublicCompany;
+	@FXML
+	ChoiceBox<Sector> SectorPublicCompany;
 	@FXML
 	ImageView logoPublic;
 	@FXML
-	Pane CompanyPane;
+	Button openButtonPublic;
+	static Company publicCompany =new Company();
+	static Bond bond=new Bond();
+	
+	
+	//*****************end form add company public*******************
+	//*****************start form add company  private *********************
+	@FXML
+	Button addCompanyPrivateB;
+	@FXML
+	TextField namePrivateCompany;
+	@FXML
+	DatePicker DatePrivateCompany;
+	@FXML
+	TextField CodePrivateCompany;
+	@FXML	
+	TextField headPrivateCompany;
+	@FXML
+	TextField PhonePrivateCompany;
+	@FXML
+	TextField socialPrivateCompany;
+	@FXML
+	ChoiceBox<Sector> SectorPrivateCompany;
+	@FXML
+	ImageView logoPrivate;
+	@FXML
+	Button openButtonPrivate;
+	@FXML
+	TextField nbSahre;
+	@FXML
+	TextField shareValue;
+	
+	
+	static Company privateCompany =new Company();
+	static Share privateShare=new Share();
+	
+	
+	//*****************end form add company private*******************
+	//*****************start form add Bank *********************
+		@FXML
+		Button addBankB;
+		@FXML
+		TextField nameBank;
+		@FXML
+		DatePicker DateBank;
+		@FXML
+		TextField CodeBank;
+		@FXML
+		TextField headBank;
+		@FXML
+		TextField PhoneBank;
+		@FXML
+		TextField socialBank;
+		
+		@FXML
+		ImageView logoBank;
+		@FXML
+		Button openButtonBank;
+		@FXML
+		TextField bankNbSahre;
+		@FXML
+		TextField bankShareValue;
+		
+		static Bank bank =new Bank();
+		static Share shareBank=new Share();
+		
+		
+		//*****************end form add Bank*******************
+	CompanyShareService remoteCompanyService;
+	BankShareServices remoteBankServices;
+	
+	public CompanyShareService getCompanyContext() {
+		try {
+			Context context = new InitialContext();
+			remoteCompanyService = (CompanyShareService) context
+					.lookup("Blues/CompanyShareServicesImpl!tn.esprit.Blues.Services.CompanyShareService");
+			return remoteCompanyService;
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return remoteCompanyService;
+		}
+
+	}
+	public BankShareServices getBankContext() {
+		try {
+			Context context = new InitialContext();
+			remoteBankServices = (BankShareServices) context
+					.lookup("Blues/BankShareServicesImp!tn.esprit.Blues.Services.BankShareServices");
+			return remoteBankServices;
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return remoteBankServices;
+		}
+
+	}
+	
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-
+		
+	     
+	           
+	            
+	        
 	}
 
-	@FXML
-	ImageView home;
+	
 
 	public void zoomHome() {
 
@@ -311,6 +445,75 @@ public class companyCTRL implements Initializable, ControlledScreen {
 
 		}
 
+	}
+	@FXML
+	public void addCompanyPublic(ActionEvent event){
+		//c.setDateIncorporation(DatePublicCompany.get);
+		System.out.println("add1");
+		System.out.println(headPublicCompany.getText());
+		publicCompany.setHeadOffice(headPublicCompany.getText());
+		System.out.println("add2");
+		publicCompany.setName(namePublicCompany.getText());
+		System.out.println("add3");
+		publicCompany.setNature("public");
+		System.out.println("add3");
+		publicCompany.setPhone(Integer.parseInt(PhonePublicCompany.getText()));
+		System.out.println("add4");
+		publicCompany.setSocialCapital(Float.parseFloat(socialPublicCompany.getText()));
+		System.out.println("hhello");
+		this.getCompanyContext().add(publicCompany);
+		
+		
+		
+		
+	}
+	@FXML
+	public void addCompanyPrivate(ActionEvent event){
+		//c.setDateIncorporation(DatePublicCompany.get);
+		System.out.println("add1");
+		System.out.println(headPrivateCompany.getText());
+		privateCompany.setHeadOffice(headPrivateCompany.getText());
+		System.out.println("add2");
+		privateCompany.setName(namePrivateCompany.getText());
+		System.out.println("add3");
+		privateCompany.setNature("private");
+		System.out.println("add3");
+		privateCompany.setPhone(Integer.parseInt(PhonePrivateCompany.getText()));
+		System.out.println("add4");
+		privateCompany.setSocialCapital(Float.parseFloat(socialPrivateCompany.getText()));
+		System.out.println("add5");
+		privateCompany.setNbShares(Integer.parseInt(nbSahre.getText()));
+		
+		this.getCompanyContext().add(privateCompany);
+		
+		
+		
+		
+	}
+	@FXML
+	public void addBankPrivate(ActionEvent event){
+		//c.setDateIncorporation(DatePublicCompany.get);
+		System.out.println("add1");
+		System.out.println(headBank.getText());
+		bank.setHeadOffice(headBank.getText());
+		System.out.println("add2");
+		bank.setName(nameBank.getText());
+		System.out.println("add3");
+		bank.setNature("Bank");
+		System.out.println("add3");
+		bank.setPhone(Integer.parseInt(PhoneBank.getText()));
+		System.out.println("add4");
+		bank.setSocialCapital(Float.parseFloat(socialBank.getText()));
+		System.out.println("add5");
+		bank.setNbShares(Integer.parseInt(bankNbSahre.getText()));
+		shareBank.setCompany(bank);
+		shareBank.setOpningPrice(Float.parseFloat(bankShareValue.getText()));
+		
+		this.getBankContext().add(bank,shareBank);
+		
+		
+		
+		
 	}
 
 	@FXML
